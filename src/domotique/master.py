570d225456dac21cc92eba31c2c8b7f6cae59b50
Unknown <albouy.romain@gmail.com>
2018-09-14 16:17:57 +0200

    Rennomage

--- src/domotique/master.py
+++ src/domotique/master.py
@@ -8,11 +8,12 @@ class Master(object):
 	def __init__(self, name):
 		self.name = name
 		self.tier = 99 # 0 => leader, 99 = inactif
-		self.tierNodeList = 99*[None] # Adresse réseaux des autres tiers
-		self.rulesDataset = {} # Régle synchronisée par les tiers
+		self.tierNodeList = 99*[None] # Adresses réseaux des autres tiers
+		self.rulesDataset = {"version":0} # Régle synchronisée par les tiers (statique)
+		self.syncDataset = {"version":0} # Donnée d'état repliqué par les tiers (dynamique)
 		
 	def Promote(self):
-		if self.tier == 0:
+		if self.tier <= 0:
 			return False
 		else:
 			self.tier -= 1
